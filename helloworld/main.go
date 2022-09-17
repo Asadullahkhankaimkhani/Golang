0e5dfc171eb73009910c502251cba32d43b78ecc
murtaza <murtaza.hussian@inserito.com>
2022-09-17 20:58:37 +0500

    Functions and variables

--- helloworld/main.go
+++ helloworld/main.go
@@ -5,4 +5,19 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 
+	var whattoSay string
+	var i int
+
+	whattoSay = "Hello, World!"
+	fmt.Println(whattoSay)
+	i = 1
+	fmt.Println(i)
+	unExported, unExpected2 := twoReturn()
+
+	fmt.Println(unExported)
+	fmt.Println(unExpected2)
+}
+
+func twoReturn() (int, int) {
+	return 1, 2
 }
