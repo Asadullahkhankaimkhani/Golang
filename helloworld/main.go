33c096e17b1cacbb6b092ca4da24fa58c5cc23ba
murtaza <murtaza.hussian@inserito.com>
2022-09-18 18:20:35 +0500

    Type and Structs

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,37 +1,57 @@
 package main
 
-import "fmt"
+import "log"
+
+var s = "Hello"
+
+type User struct {
+	Name string
+	Age  int
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	// fmt.Println("Hello, World!")
 
-	var whattoSay string
-	var i int
+	// var whattoSay string
+	// var i int
 
-	whattoSay = "Hello, World!"
-	fmt.Println(whattoSay)
-	i = 1
-	fmt.Println(i)
-	unExported, unExpected2 := twoReturn()
+	// whattoSay = "Hello, World!"
+	// fmt.Println(whattoSay)
+	// i = 1
+	// fmt.Println(i)
+	// unExported, unExpected2 := twoReturn()
 
-	fmt.Println(unExported)
-	fmt.Println(unExpected2)
+	// fmt.Println(unExported)
+	// fmt.Println(unExpected2)
 
-	var myString string
-	myString = "Green"
+	// var myString string
+	// myString = "Green"
 
-	fmt.Println(myString)
+	// fmt.Println(myString)
 
-	changeUsingPointer(&myString)
+	// changeUsingPointer(&myString)
 
-	fmt.Println(myString)
+	// fmt.Println(myString)
+
+	// var s2 = "six"
+
+	//var s = "five"
+
+	//log.Println(s2)
+	//log.Println(s)
+
+	var user User
+	user.Name = "John"
+	user.Age = 30
+
+	log.Println(user)
 
 }
 
-func changeUsingPointer(myString *string) {
-	*myString = "Red"
-}
+// func changeUsingPointer(myString *string) {
+// 	*myString = "Red"
+// }
 
-func twoReturn() (int, int) {
-	return 1, 2
-}
+// func twoReturn() (int, int) {
+// 	return 1, 2
+// }
